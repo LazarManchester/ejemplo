c98943959b96d8806bb42f39fb92806b8043f571
Lazar <Lazar>
2023-05-28 23:23:22 +0200

    hola

---
+++ ejemplogit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author LAZAR
+ *
+ */
+module ejemplogit {
+}
---
+++ ejemplogit/src/pruebaGit/PruebaGit01.java
@@ -0,0 +1,11 @@
+package pruebaGit;
+
+public class PruebaGit01 {
+
+	public static void main(String[] args) {
+		System.out.println("esto es una prueba para git hub");
+		
+
+	}
+
+}
